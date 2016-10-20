866ae358723e34d2c3efe367f85e664e0220b222
Peter <flantilio@gmail.com>
2016-10-20 18:46:46 +0200

    Points, Segments, and Lines as namedtuples

--- Raspberry/map.py
+++ Raspberry/map.py
@@ -1,5 +1,13 @@
 import cocos
 import json
+from collections import namedtuple
+
+WHITE = (255, 255, 255, 255)
+RED = (255, 0, 0, 255)
+
+Point = namedtuple('Point', ['x', 'y'])
+Line = namedtuple('Line', ['a', 'c'])
+Segment = namedtuple('Segment', ['A', 'B'])
 
 
 def ReadMap(file):
@@ -8,50 +16,40 @@ def ReadMap(file):
     return data
 
 
-def Interception(A, B):
-    a1, c1, a2, c2 = A[0], A[1], B[0], B[1]
-    if (a2 - a1) is 0:
+def Interception(A: Line, B: Line) -> Point:
+    if (B.a - A.a) is 0:
         return None
-    x = (c1 - c2)/(a2 - a1)
-    y = a1 * x + c1
-    return x, y
+    x = (A.c - B.c)/(B.a - A.a)
+    y = A.a * x + A.c
+    return Point(x, y)
 
 
-def PointsToFactors(A, B):
-    x1, y1, x2, y2 = A[0], A[1], B[0], B[1]
-    if (x2 - x1) is 0:
+def PointsToFactors(A: Point, B: Point) -> Line:
+    if (B.x - A.x) is 0:
         return None
-    a = (y2 - y1)/(x2 - x1)
-    c = y1 - a * x1
-    return a, c
+    a = (B.y - B.x)/(A.x - B.x)
+    c = A.y - a * A.x
+    return Line(a, c)
 
 
-def Midpoint(A, B):
-    x1, y1, x2, y2 = A[0], A[1], B[0], B[1]
-    x, y = x2 - x1, y2 - y1
-    return x, y
+def Midpoint(A: Point, B: Point) -> Point:
+    x, y = A.x + (B.x - A.x)/2, A.y + (B.y - A.y)/2
+    return Point(x, y)
 
 
 class MapDisplay(cocos.layer.Layer):
     is_event_handler = True
+    center = 327, 136
 
     def __init__(self):
         super(MapDisplay, self).__init__()
         self.lines = ReadMap('map.json')
-        center = 327, 136
+        self.center = 327, 136
         for line in self.lines:
-            fac1 = PointsToFactors(line[0], line[1])
-            fac2 = PointsToFactors(center, Midpoint(line[0], line[1]))
-            if fac1 and fac2 is not None:
-                int = Interception(fac1, fac2)
-                if int is not None:
-                    self.add(cocos.draw.Line(center, int,
-                                             (255, 255, 255, 255),
-                                             stroke_width=2))
-            self.add(cocos.draw.Line(line[0], line[1],
-                                     (255, 255, 255, 255),
-                                     stroke_width=5))
-
+            seg = Segment(Point._make(line[0]), Point._make(line[1]))
+            mid = Midpoint(seg.A, seg.B)
+            self.add(cocos.draw.Line(self.center, mid, RED, stroke_width=3))
+            self.add(cocos.draw.Line(seg.A, seg.B, WHITE, stroke_width=5))
 
 
 cocos.director.director.init()
