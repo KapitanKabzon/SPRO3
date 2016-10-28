3d184317260b1225dce68545e0016f45dbf6faf5
Peter <flantilio@gmail.com>
2016-10-28 02:50:50 +0200

    things

--- Raspberry/bellhop.py
+++ Raspberry/bellhop.py
@@ -1,9 +1,10 @@
-from math import sin, cos
+from math import sin, cos, atan2
 import serial
 
 
 class Ping():
     def __init__(self, r, beta, gamma, arduino=None, id=None):
+        self.id = id
         self.r = r
         self.beta = beta
         self.gamma = gamma
@@ -24,17 +25,55 @@ class Ping():
 
 
 class Bellhop():
-    def __init__(self, x, y, alpha):
+    def __init__(self, x, y, alpha, arduino=None):
         self.x = x
         self.y = y
+        # Bearing angle
         self.alpha = alpha
-        self.arduino = Arduino()
+        self.arduino = arduino
+        # List of attached ultrasonic sensors
+        self.sensors = []
+
+    def sensor_point(self, sensor: Ping):
+        """Return x, y coordinates of a sensor with respect to
+        the bellhop position"""
+        x = self.x + sensor.r * sin(sensor.beta + self.alpha)
+        y = self.y + sensor.r * cos(sensor.beta + self.alpha)
+        return x, y
+
+    def sensor_measurement(self, sensor: Ping):
+        """Return x, y coordinates of measured point with respect to
+        the bellhop position"""
+        xs, ys = self.sensor_point(sensor)
+        x = xs + sensor.d * sin(sensor.gamma + self.alpha)
+        y = ys + sensor.d * cos(sensor.gamma + self.alpha)
+        return x, y
+
+    def update_position(self, x, y):
+        '''Updates position of bellhop based on new position x, y'''
+        self.x = x
+        self.y = y
+
+    def deltas(self, x, y):
+        """Update position and return change in position and angle"""
+        # Compute change in the position
+        dx, dy = x - self.x, y - self.y
+
+        # Compute change in the bearing angle
+        # atan2() returns the angle in the correct quadrant
+        theta = atan2(dy, dx)
+        dalpha = theta - self.alpha
+
+        # Update position
+        self.update_position(x, y)
+        return dx, dy, dalpha
 
 
 class Arduino():
     def __init__(self, PORT):
-        self.serial = serial.Serial(PORT)
-        self.reading = []
+#        self.serial = serial.Serial(PORT)
+#        self.reading = []
+        pass
 
     def sensor_data(self, id):
         pass
@@ -45,8 +84,3 @@ class Arduino():
 
     def close(self):
         self.serial.close()
-
-
-if False:
-    arduino = Arduino('/dev/ttyUSB0')
-    arduino.close()
--- Raspberry/map.py
+++ Raspberry/map.py
@@ -1,17 +1,19 @@
 from tkinter import Canvas, Tk, StringVar
 from tkinter.ttk import Frame, Button, Entry, Label
-from math import pi, cos, sin
+from math import pi, atan2, sin, cos
 import bellhop as hop
 
 
 class Application(Frame):
     def __init__(self, master=None):
+        self.width = 800
+        self.height = 600
         Frame.__init__(self, master)
         pad = 5
         self.grid()
 
         # Map canvas
-        self.canvas = Canvas(self, width=800, height=600)
+        self.canvas = Canvas(self, width=self.width, height=self.height)
         self.canvas.grid(row=0, column=0, columnspan=4, padx=pad, pady=pad)
 
         # Add button
@@ -30,55 +32,107 @@ class Application(Frame):
         # Status label
         self.status = Label(self)
         self.status.grid(row=1, column=2, padx=pad, pady=pad)
-
         self.entry.focus()
 
         # Bellhop biz
-        self.ping = hop.Ping(45, 0.15*pi, 0.5 * pi)
-        self.ping.d = 0
-        self.r = 80
+        self.bellhop = hop.Bellhop(800/2, 600/2, 0)
+        self.add_ping(40, 0, 0, id=0)
+        self.add_ping(40, 0.125*pi, 0.125*pi, id=1)
+        self.add_ping(40, -0.125*pi, -0.125*pi, id=2)
+        self.add_ping(40, 1*0.25*pi, 0.5*pi, id=3)
+        self.add_ping(40, 3*0.25*pi, 0.5*pi, id=4)
+        self.add_ping(40, 5*0.25*pi, -0.5*pi, id=5)
+        self.add_ping(40, 7*0.25*pi, -0.5*pi, id=6)
+        self.add_ping(40, pi, pi, id=7)
+
+        cords = (self.bellhop.x - 2,
+                 self.bellhop.y - 2,
+                 self.bellhop.x + 2,
+                 self.bellhop.y + 2
+                 )
+        self.canvas.create_rectangle(*cords, fill='white', tags='hop')
+
+        self.readings = []
+        for sensor in self.bellhop.sensors:
+            sensor.d = 50
+            x1, y1 = self.bellhop.sensor_point(sensor)
+            x2, y2 = self.bellhop.sensor_measurement(sensor)
+            cords = x1, y1, x2, y2
+            cords = self.inversey(cords)
+            self.canvas.create_line(*cords,
+                                    fill='blue',
+                                    width=3,
+                                    tags='hop'
+                                    )
+            cords = self.bellhop.x, self.bellhop.y, x1, y1
+            cords = self.inversey(cords)
+            line = self.canvas.create_line(*cords, fill='red', tags='hop')
+            self.readings.append(line)
+
+        self.update_map()
+        self.r = 90
         self.angle = 0
-        self.ping.d = 30
-        xoff = 800/2 + self.r * cos(self.angle)
-        yoff = 600/2 + self.r * sin(self.angle)
-        x1, y1 = self.ping.point()
-        x2, y2 = self.ping.measurement_point()
-        x1 += xoff
-        x2 += xoff
-        y1 += yoff
-        y2 += yoff
-        self.xoff = xoff
-        self.yoff = yoff
-        print(x1, y1, x2, y2)
-        cords = (x1, y1, x2, y2)
-        self.ping_line = self.canvas.create_line(*cords,
-                                                 fill='red',
-                                                 width=1,
-                                                 activewidth=4)
+
+        # Start the loop
         self.onUpdate()
+
+    def update_map(self):
+        """Update map canvas"""
+        # Store old position and bearing of bellhop
+        x, y, alpha = self.bellhop.x, self.bellhop.y, self.bellhop.alpha
+
+        # Get change
+#        dx, dy, dalpha = self.bellhop.deltas()
+
+        # Delete old lines
+        self.canvas.delete('hop')
+
+        # Redraw bellhop
+        for sensor in self.bellhop.sensors:
+            sensor.d = 50
+            x1, y1 = self.bellhop.sensor_point(sensor)
+            x2, y2 = self.bellhop.sensor_measurement(sensor)
+            cords = x1, y1, x2, y2
+            cords = self.inversey(cords)
+            self.canvas.create_line(*cords,
+                                    fill='blue',
+                                    width=3,
+                                    tags='hop'
+                                    )
+            cords = self.bellhop.x, self.bellhop.y, x1, y1
+            cords = self.inversey(cords)
+            line = self.canvas.create_line(*cords, fill='red', tags='hop')
+            self.readings.append(line)
+
+    def newpos(x, y):
+        return x, y
 
     def entry_handler(self, event):
         if self.focus_get() is self.entry:
-            print(self.entry_contents.get())
+            try:
+                input = self.entry_contents.get().split()
+                self.canvas.create_line(*input, fill='red', width=3)
+            except:
+                print('')
+            self.entry_contents.set('')
+
+    def add_ping(self, r, beta, gamma, id=None):
+        ping = hop.Ping(r, beta, gamma, id)
+        self.bellhop.sensors.append(ping)
+
+    def inversey(self, cords):
+        y1 = self.height - cords[1]
+        y2 = self.height - cords[3]
+        new = cords[0], y1, cords[2], y2
+        return new
 
     def onUpdate(self):
-        old = self.canvas.coords(self.ping_line)
-        old_point = old[2], old[3]
-        step = 2*pi/(30)
-        step2 = 2*pi/(pi*25)
-        self.angle += step2
-        self.xoff = 800/2 + self.r * cos(self.angle)
-        self.yoff = 600/2 + self.r * sin(self.angle)
-        self.reverse = False
-        self.ping.gamma += step
-        point = self.ping.measurement_point()
-        new_point = point[0] + self.xoff, point[1] + self.yoff
-        cords = old_point[0], old_point[1], new_point[0], new_point[1]
-        self.canvas.create_line(*cords, fill='yellow', width=1)
-        new = old[0], old[1], point[0] + self.xoff, point[1] + self.yoff
-        self.canvas.coords(self.ping_line, new)
-        self.after(1, self.onUpdate)
-
+        self.bellhop.x = 800/2 + self.r * sin(self.angle)
+        self.bellhop.y = 600/2 + self.r * cos(self.angle)
+        self.bellhop.alpha = self.angle + (2*pi)/4
+        self.angle += (2*pi)/500
+        self.update_map()
+        self.after(10, self.onUpdate)
 
 root = Tk()
 app = Application(master=root)
