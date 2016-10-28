3ec28291a52becae423470c1177ce5bcc27fe83e
Catherine Beryl Basson <cbcbasson@wildhippo.ca>
2016-10-28 21:42:41 +0200

    communication to usart upon request

--- Test/MultipleUltrasonicTst/MultipleUltrasonicTst/main.c
+++ Test/MultipleUltrasonicTst/MultipleUltrasonicTst/main.c
@@ -4,11 +4,12 @@
  * Created: 24/10/2016 14:35:45
  *  Author: Piotr Chromi?ski
  *  Edited by: Catherine Beryl Basson
- */ 
-#define F_CPU 16E6
+ */
+#define F_CPU 16000000UL
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/cpufunc.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <util/delay.h>
@@ -26,17 +27,24 @@
 #define ECHO2_REG PCINT20
 #define SENSORS 3
 
-/* #define for max number of clock cycles before max distance, and the time
- * duration in microseconds of one clock cycle */
-#define MAX_PING 376183
-#define US_PER_CLK 0.
+/* #define for the time duration in microseconds of one clock cycle,
+* 2x microsecond per centimetre of speed of sound, max number of clock
+* cycles before max distance */
+#define US_PER_CLK 0.0625
+#define US_PER_CM_2 58.275
+#define MAX_PING ((400 * US_PER_CM_2) / US_PER_CLK)
+
+/* Function prototypes */
+/* Functions for initialisation */
 void sensor_init(void);
 void interrupt_init(void);
+
 void interrupt_echo_enable(void);
 void interrupt_echo_disable(void);
 float get_distance(void);
 
 volatile int flag = 0;
+volatile int usart_check = 0;
 volatile uint32_t count = 0;
 volatile char sensor[] = {ECHO0, ECHO1, ECHO2};
 volatile char sensor_reg[] = {ECHO0_REG, ECHO1_REG, ECHO2_REG};
@@ -48,86 +56,35 @@ int main(void)
 	io_redirect();
 	sensor_init();
 	interrupt_init();
+	sei();
 	
 	float distance[SENSORS] = {0.00};
 	
 	while (1){
 		distance[current_sensor] = get_distance();
-		printf("Sensor %d: %.2f\n", current_sensor, distance[current_sensor]);
-		current_sensor++;
 		
+		if (usart_check){
+			for(int i = 0; i < SENSORS; i++){
+				printf("Sensor %d: %.2f\n", i, distance[i]);
+			}
+			usart_check = 0;
+		}
+		
+		current_sensor++;
 		if (current_sensor > (SENSORS - 1))
-			//printf("\n");
 			current_sensor = 0;
-		_delay_us(1000000);
+		_delay_ms(100);
 	}
 }
 
-void sensor_init(void)
-{
-	/* Sets trigger as output, and clears so it outputs nothing */
-	DDRB |= _BV(TRIGGER);		
-	PORTB &= ~(_BV(TRIGGER));
-	
-	/* Sets ECHO pins as input */
-	DDRD &= ~(_BV(ECHO0));
-	DDRD &= ~(_BV(ECHO1));
-}
-
-void interrupt_init(void)
-{
-	/* Set PCIE2 to enable PCMSK2 scan (PD0 - PD7) */
-	PCICR |= _BV(PCIE2);
-}
-
-float get_distance(void)
-{
-	interrupt_echo_enable();
-	PORTB &= ~(_BV(TRIGGER));
-	sei();
-	
-	/* Send a pulse */
-	_delay_us(20);
-	PORTB |= _BV(TRIGGER);
-	_delay_us(12);
-	PORTB &= ~(_BV(TRIGGER));
-	
-	/* Wait until echo is done receiving pulse */
-	flag = 1;
- 	while (flag);
-		 
-	/* Disable echo pin from reading */
-	cli();
-	interrupt_echo_disable();
-
-	/* Check that maximum number of clock cycles hasn't been exceeded */
-	if (count > MAX_PING || count == 0)
-		count = MAX_PING;
-	
-	float dist = (double) count * US_PER_CLK / 58.275; /* pulse width time / ((microseconds per centimetre for speed of sound) * 2) */ 
-	count = 0;
-	return dist;
-}
-
-void interrupt_echo_enable(void)
-{
-	/* Trigger interrupt on level change */
-	PCMSK2 |= _BV(sensor_reg[current_sensor]);
-}
-
-void interrupt_echo_disable(void)
-{
-	/* Disable interrupt for pin */
-	PCMSK2 &= ~(_BV(sensor_reg[current_sensor]));
-}
-
+/* Interrupt service routines */
 ISR (PCINT2_vect)
 {
 	if ((PIND & _BV(sensor[current_sensor]))) {
 		/* Set up and start TIMER0 when echo receives pulse*/
-		TIMSK0 |= (1 << TOIE0);
+		TIMSK0 |= _BV(TOIE0);
 		TCCR0B |= _BV(CS00);
-	} else {
+		} else {
 		/* Stop timer and reset flag when pulse is done */
 		TCCR0B &= ~(_BV(CS00));
 		TIMSK0 &= ~(_BV(TOIE0));
@@ -149,4 +106,81 @@ ISR (TIMER0_OVF_vect)
 		count += TCNT0;
 		TCNT0 = 0;
 	}
+}
+
+
+ISR (USART_RX_vect)
+{
+	if(UDR0)
+		usart_check = 1;
+}
+
+/* Function definitions */
+/* Initialise trigger and echo pins on the sensors */
+void sensor_init(void)
+{
+	/* Sets trigger as output, and clears so it outputs nothing */
+	DDRB |= _BV(TRIGGER);		
+	PORTB &= ~(_BV(TRIGGER));
+	
+	/* Sets ECHO pins as input */
+	DDRD &= ~(_BV(ECHO0));
+	DDRD &= ~(_BV(ECHO1));
+	DDRD &= ~(_BV(ECHO2));
+}
+
+/* Initialise interrupt on pins PCINT[23:16] */
+void interrupt_init(void)
+{
+	/* Set PCIE2 to enable PCMSK2 scan (PD0 - PD7) */
+	PCICR |= _BV(PCIE2);
+	
+	/* Enable USART receive complete interrupt */
+	UCSR0B |= _BV(RXCIE0);
+}
+
+/* Sends pulse through trigger and receives pulse width in echo, then
+ * calculates the distance and returns it to main() */
+float get_distance(void)
+{
+	cli();
+	interrupt_echo_enable();
+	PORTB &= ~(_BV(TRIGGER));
+	sei();
+	
+	/* Send a pulse */
+	_delay_us(20);
+	PORTB |= _BV(TRIGGER);
+	_delay_us(12);
+	PORTB &= ~(_BV(TRIGGER));
+	
+	/* Wait until echo is done receiving pulse */
+	flag = 1;
+ 	while (flag);
+		 
+	/* Disable echo pin from reading */
+	cli();
+	interrupt_echo_disable();
+	sei();
+	/* Check that maximum number of clock cycles hasn't been exceeded */
+	if (count > MAX_PING || count == 0)
+		count = MAX_PING;
+	
+	 /* pulse width time / ((microseconds per centimetre for speed of sound) * 2) */ 
+	float dist = (double) count * US_PER_CLK / US_PER_CM_2;
+	count = 0;
+	
+	return dist;
+}
+
+/* Trigger interrupt on level change for pin/register of current sensor */
+void interrupt_echo_enable(void)
+{
+	PCMSK2 |= _BV(sensor_reg[current_sensor]);
+}
+
+/* Disables interrupt for pin/register of current sensor */
+void interrupt_echo_disable(void)
+{
+	PCMSK2 &= ~(_BV(sensor_reg[current_sensor]));
 }
